eee4bcd139a75697b28c60b3cf5f1a45c6bb5007
sstathatos <stefanos.stathatos@earthanalytics.no>
2021-08-30 00:35:27 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# File manager app
+## Basic file manager application to practice on interesting skills
+Commands to reproduce:
+1. python -m venv file-manager
+2. workon file-manager
+3. django-admin startproject filemanager
---
+++ filemanager/filemanager/models.py
@@ -0,0 +1,20 @@
+from django.core.files.storage import FileSystemStorage
+from django.core.validators import FileExtensionValidator
+from django.db import models
+from django.utils import timezone
+
+ALLOWED_FILE_EXTENSIONS = ["pdf", "txt", "jpg", "jpeg", "mp4"]
+
+
+class File(models.Model):
+    file_id = models.AutoField(primary_key=True)
+    file = models.FileField(
+        max_length=255,
+        validators=[FileExtensionValidator(allowed_extensions=ALLOWED_FILE_EXTENSIONS)],
+        storage=FileSystemStorage(),
+    )
+    date_created = models.DateTimeField(default=timezone.now)
+    # todo fix this
+
+    def __str__(self):
+        return str(self.file.name)
