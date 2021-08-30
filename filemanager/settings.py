502c2f1875798973fa8c0c4bed150d466fd942d8
sstathatos <stefanos.stathatos@earthanalytics.no>
2021-08-30 22:01:50 +0300

    changed base dir and media path

--- filemanager/settings.py
+++ filemanager/settings.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 from pathlib import Path
 from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -126,6 +127,6 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 ALLOWED_HOSTS: List[str] = []
--- filemanager/viewsets.py
+++ filemanager/viewsets.py
@@ -1,9 +1,23 @@
-from rest_framework import viewsets
+import os
+
+from django.http import HttpResponse
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
 from .models import File
 from .serializers import FileSerializer
+from .settings import MEDIA_ROOT
 
 
 class FileViewSet(viewsets.ModelViewSet):
     queryset = File.objects.all()
     serializer_class = FileSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = FileSerializer(instance=instance)
+        try:
+            with open(os.path.join(MEDIA_ROOT, serializer.data["name"]), "rb") as f:
+                return HttpResponse((f.read()), content_type="image/png")
+        except IOError:
+            return Response(status=status.HTTP_404_NOT_FOUND)
